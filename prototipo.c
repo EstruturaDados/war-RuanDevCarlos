551b43124d0a43d246a29fd416c935d320d38931
ruandevcarlos <ruan.dev.carlos@gmail.com>
2026-07-15 11:33:56 -0300

    começo da função atacar

--- prototipo.c
+++ prototipo.c
@@ -19,32 +19,46 @@ void limparBufferEntrada() {
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
+// função de ataque
+void atacar(struct territorio *atacante, struct territorio *defensor) {
+    srand(time(NULL));
+    int numATK = 1 + (rand() % 6);
+    int numDEF = 1 + (rand() % 6);
+    printf("\n%d, %d", numDEF, numATK);
+}
+
+// função principal
 int main() {
     int i;
     int opcao;
     int MAX_TERRITORIO;
-    int *quant_territorio;
-
 
     //MENU
     do {
         printf("\n========================================================\n");
         printf("\n");
         printf("Insira a Quantidade de Territorios: ");
-        scanf("%d", quant_territorio);
+        scanf("%d", &MAX_TERRITORIO);
         limparBufferEntrada();
 
-        MAX_TERRITORIO = *quant_territorio;
-        struct territorio mapa[MAX_TERRITORIO];
+        // criando o vetor mapa do tipo territorio, com alocação dinamica
+        struct territorio *mapa = (struct territorio *) malloc(MAX_TERRITORIO * sizeof(struct territorio));
+
+        if (mapa == NULL) {
+            printf("\nERRO: MEMORIA INSUFICIENTE!\n");
+        };
+        
 
         // cadastro de territórios
         for (i = 0; i < MAX_TERRITORIO; i++) {
             printf("\n--- Cadastrando Territorio %d ---\n", i + 1);
             printf("Nome do Territorio: ");
             fgets(mapa[i].nome, 30, stdin);
+            mapa[i].nome[strcspn(mapa[i].nome, "\n")] = '\0';
 
             printf("Cor do exercito: ");
             fgets(mapa[i].cor, 10, stdin);
+            mapa[i].cor[strcspn(mapa[i].cor, "\n")] = '\0';
 
             printf("Numero de tropas: ");
             scanf("%d", &mapa[i].tropas);
@@ -58,12 +72,12 @@ int main() {
 
         // exibição de territórios cadastrados
         for (i = 0; i < MAX_TERRITORIO; i++) {
-            printf("\nTerritorio %d:", i + 1);
-            printf("- Nome: %s", mapa[i].nome);
-            printf("- Cor: %s", mapa[i].cor);
-            printf("- Tropas: %d\n", mapa[i].tropas);
-        };
+            printf("%d. %s - Cor: %s | Tropas: %d\n", i + 1, mapa[i].nome, mapa[i].cor, mapa[i].tropas);
+        }
 
+        atacar(mapa, mapa);
+
+        free(mapa);
         opcao = 0;
     } while (opcao != 0);
     
